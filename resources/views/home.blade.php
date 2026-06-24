f8361095d0337db8b36eacc569a4ea2c659bc051
NimaAhmadi <mrnima2920@gmail.com>
2026-06-24 15:52:13 +0330

    Add HomeController and home page route
    - Create HomeController with index method
    - Update home route to use controller
    - Create basic home.blade.php view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'نیما احمدی - توسعه‌دهنده Laravel')
+
+@section('content')
+    <h1>صفحه اصلی</h1>
+    <p>اینجا بعداً بخش‌های Hero، About، Skills و Contact میاد.</p>
+@endsection
